ede3a22627ac08a2e4a0842e1860169bc2bc46e3
bonneyhaha <loyalka.harsh24@gmail.com>
2024-09-01 15:41:40 +0530

    final commit

---
+++ server/utils/mailSender.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer")
+
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: process.env.MAIL_SECURE === 'true', // true for 465, false for other ports
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false, // Necessary for some setups
+      },
+    });
+    console.log("Transporter configuration:", transporter.options);
+    let info = await transporter.sendMail({
+      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`, // sender address
+      to: `${email}`, // list of receivers
+      subject: `${title}`, // Subject line
+      html: `${body}`, // html body
+    })
+    console.log(info.response)
+    return info
+  } catch (error) {
+    console.log(error.message)
+    return error.message
+  }
+}
+
+module.exports = mailSender
---
+++ src/ZZtest/App.js
@@ -0,0 +1,89 @@
+/* eslint-disable no-unused-vars */
+import "./App.css";
+import {Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import Navbar from "./components/common/Navbar"
+import OpenRoute from "./components/core/Auth/OpenRoute"
+
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import { FcAbout } from "react-icons/fc";
+import About from "./pages/About"
+import MyProfile from "./components/core/Dashboard/MyProfile";
+
+function App() {
+  return (
+   <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+    <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+    <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword/>
+            </OpenRoute>
+          }
+        />
+    <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+    <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+    {/* </Routes> */}
+    <Route
+          path="about"
+          element={
+            <OpenRoute>
+              <About />
+            </OpenRoute>
+          }
+        />
+     <Route
+          path="dashboard/my-profile"
+          element={
+            <OpenRoute>
+              {<MyProfile/>}
+             </OpenRoute>
+          }
+        />
+
+
+
+    </Routes>
+
+   </div>
+  );
+}
+
+export default App;
